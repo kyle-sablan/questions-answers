9a42309d3335202f6eb64604a4692e5f47db58a7
kyle_sablan <kyle.sablan@gmail.com>
2021-12-01 12:15:47 -0800

    Added in security by moving private info into .env file and using those variables to create connection to database server

--- database/index.js
+++ database/index.js
@@ -1,6 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
+  host: process.env.HOST,
+  port: process.env.PORT,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
   database: 'questions_and_answers'
 });
 
