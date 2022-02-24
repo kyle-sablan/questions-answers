5f8059009d120f2d350e654975c360fb624e049d
kyle_sablan <kyle.sablan@gmail.com>
2022-02-23 21:46:37 -0800

    Testing adjustments to correctly retrieve all data. current path of using lateral joins might be useful

--- database/index.js
+++ database/index.js
@@ -1,12 +1,19 @@
-require('dotenv').config();
-const { Pool } = require('pg');
+require("dotenv").config();
+const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   host: process.env.HOST,
+//   port: 5432,
+//   user: 'api_user',
+//   password: process.env.PASSWORD,
+//   database: 'questions_and_answers'
+//   // ssl: 1
+// });
 
 const pool = new Pool({
   host: process.env.HOST,
   port: 5432,
-  user: 'api_user',
-  password: process.env.PASSWORD,
-  database: 'questions_and_answers'
+  database: "questions_and_answers",
   // ssl: 1
 });
 
